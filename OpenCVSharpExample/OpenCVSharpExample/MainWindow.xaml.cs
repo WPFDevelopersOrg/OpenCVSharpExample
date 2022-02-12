d1fd112f780dad7cb644112c444db884ff2c26d3
yanjinhuagood <yanjinhua@gmail.com>
2022-02-12 20:05:18 +0800

    add in OpenCVSharpExample.

---
+++ OpenCVSharpExample/OpenCVSharpExample/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Management;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace OpenCVSharpExample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        private VideoCapture capCamera;
+        private Mat matImage = new Mat();
+        private Thread cameraThread;
+
+        public List<string> CameraArray
+        {
+            get { return (List<string>)GetValue(CameraArrayProperty); }
+            set { SetValue(CameraArrayProperty, value); }
+        }
+
+        public static readonly DependencyProperty CameraArrayProperty =
+            DependencyProperty.Register("CameraArray", typeof(List<string>), typeof(MainWindow), new PropertyMetadata(null));
+
+
+
+        public int CameraIndex
+        {
+            get { return (int)GetValue(CameraIndexProperty); }
+            set { SetValue(CameraIndexProperty, value); }
+        }
+
+        public static readonly DependencyProperty CameraIndexProperty =
+            DependencyProperty.Register("CameraIndex", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Width = SystemParameters.WorkArea.Width / 1.5;
+            Height = SystemParameters.WorkArea.Height / 1.5;
+            this.Loaded += MainWindow_Loaded;
+
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeCamera();
+        }
+        private void ComboBoxCamera_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CameraArray.Count - 1 < CameraIndex)
+                return;
+
+            if (capCamera != null && cameraThread != null)
+            {
+                cameraThread.Abort();
+                StopDispose();
+            }
+
+            capCamera = new VideoCapture(CameraIndex);
+            capCamera.Fps = 30;
+            CreateCamera();
+            
+        }
+
+        private void InitializeCamera()
+        {
+            CameraArray = GetAllConnectedCameras();
+        }
+        List<string> GetAllConnectedCameras()
+        {
+            var cameraNames = new List<string>();
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE (PNPClass = 'Image' OR PNPClass = 'Camera')"))
+            {
+                foreach (var device in searcher.Get())
+                {
+                    cameraNames.Add(device["Caption"].ToString());
+                }
+            }
+
+            return cameraNames;
+        }
+
+        void CreateCamera()
+        {
+            cameraThread = new Thread(PlayCamera);
+            cameraThread.Start();
+        }
+
+        private void PlayCamera()
+        {
+            while (capCamera != null && !capCamera.IsDisposed)
+            {
+                capCamera.Read(matImage);
+                if (matImage.Empty()) break;
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                {
+                    var converted = Convert(BitmapConverter.ToBitmap(matImage));
+                    imgViewport.Source = converted;
+                }));
+            }
+        }
+       
+        private void btStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopDispose();
+            btStop.IsEnabled = false;
+        }
+
+        void StopDispose()
+        {
+            if (capCamera != null && capCamera.IsOpened())
+            {
+                capCamera.Dispose();
+                capCamera = null;
+            }
+        }
+
+        void CreateRecord()
+        {
+            cameraThread = new Thread(PlayCamera);
+            cameraThread.Start();
+        }
+
+        BitmapImage Convert(Bitmap src)
+        {
+            System.Drawing.Image img = src;
+            var now = DateTime.Now;
+            var g = Graphics.FromImage(img);
+            var brush = new SolidBrush(System.Drawing.Color.Red);
+            g.DrawString($"北京时间：{ now.ToString("yyyy年MM月dd日 HH:mm:ss")}", new System.Drawing.Font("Arial", 18), brush, new PointF(5, 5));
+            brush.Dispose();
+            g.Dispose();
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, ImageFormat.Bmp);
+            ms.Seek(0, SeekOrigin.Begin);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = ms;
+            image.EndInit();
+            image.Freeze();
+            return image;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            StopDispose();
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# OpenCVSharpExample
+
+##  Welcome to OpenCVSharpExample  
+
+![.net 4.0](https://img.shields.io/badge/.net-%3E%3D4.0-blue) ![Visual Studio 2019](https://img.shields.io/badge/Visual%20Studio%20-2019-blueviolet) 
+<a href="https://www.nuget.org/packages/WPFDevelopers.Minimal/">
+        <img alt="nuget-version" src="https://img.shields.io/nuget/v/WPFDevelopers.Minimal.svg"></img>
+    </a>  <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=B61RFy2vvpaKLEDxaW6NsDpPZA-eSyFh&jump_from=webapi"><img border="0" src="https://pub.idqqimg.com/wpa/images/group.png" alt="WPF开发者" title="WPF开发者"></a>   
+
+### [自定义高级控件](https://github.com/WPFDevelopersOrg/WPFDevelopers) https://github.com/WPFDevelopersOrg/WPFDevelopers  
+
+### [码云](https://gitee.com/WPFDevelopersOrg) https://gitee.com/WPFDevelopersOrg  
+
+
+### 欢迎关注微信公众号  
+
+<img src="https://gitee.com/WPFDevelopersOrg/ResourcesCache/raw/main/resources/wxgzh.jpg"/>  
+
+### 捐助
+如果您觉得我们的开源软件对你有所帮助，请扫下方二维码打赏我们一杯咖啡。
+| 支付宝 | 微信 |
+|----|-----|
+|  <img src="https://github.com/WPFDevelopersOrg/ResourcesCache/raw/main/resources/Alipay.png"/>   |   <img src="https://github.com/WPFDevelopersOrg/ResourcesCache/raw/main/resources/WeChatPay.png"/>   |
+
+###  效果展示  
+
+![0](https://raw.githubusercontent.com/WPFDevelopersOrg/ResourcesCache/main/resources/OpenCVSharpExampleResource/PlayCamera.gif)  
